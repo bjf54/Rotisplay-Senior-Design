6ae8a33dda1d4f099e6e54f386d686e5f5e41d9d
Kevin Curran <kec192@pitt.edu>
2022-05-25 19:58:01 -0400

    now able to simulate N rgb LEDs spinning clockwise

--- POV Fan Simulator/main.py
+++ POV Fan Simulator/main.py
@@ -1,23 +1,18 @@
 import arcade
 import math
 
-
 # Size of the screen
-SCREEN_WIDTH = 600
-SCREEN_HEIGHT = SCREEN_WIDTH
+SCREEN_SIZE = 600
 SCREEN_TITLE = "Rotisplay simulation"
 
-# Rectangle info
-RECT_WIDTH = 25
-RECT_HEIGHT = 25
-RECT_COLOR = arcade.color.WHITE
-
 BACKGROUND_COLOR = arcade.color.BLACK
+
+RGB = [arcade.color.RED, arcade.color.GREEN, arcade.color.BLUE]
 
 class Pixel:
     """ This class represents our rectangle """
 
-    def __init__(self, width, height, radius):
+    def __init__(self, radius, globalRadius, color = arcade.color.WHITE):
 
         # Set up attribute variables
 
@@ -25,29 +20,25 @@ class Pixel:
         self.center_x = 0
         self.center_y = 0
 
-        self.width = width
-
         self.radius = radius
+
+        self.globalRadius = globalRadius
         self.angle = 0
 
+        self.color = color
+
     def update(self):
-        self.angle = self.angle + 1
+        self.angle = self.angle + 5
         self.angle = self.angle % 360
 
-        self.center_x = self.radius*math.cos(math.radians(-self.angle)) + SCREEN_WIDTH/2
-        self.center_y = self.radius*math.sin(math.radians(-self.angle)) + SCREEN_HEIGHT/2
+        self.center_x = self.globalRadius*math.cos(math.radians(-self.angle)) + SCREEN_SIZE/2
+        self.center_y = self.globalRadius*math.sin(math.radians(-self.angle)) + SCREEN_SIZE/2
 
     def draw(self):
-        # Draw the rectangle
-        # arcade.draw_rectangle_filled(self.center_x,
-        #                              self.center_y,
-        #                              self.width,
-        #                              self.height,
-        #                              RECT_COLOR)
         arcade.draw_circle_filled(self.center_x,
                                     self.center_y,
-                                    self.width,
-                                    RECT_COLOR)
+                                    self.radius,
+                                    self.color)
 
 
 class Rotisplay(arcade.Window):
@@ -56,15 +47,18 @@ class Rotisplay(arcade.Window):
     def __init__(self, width, height, title):
         super().__init__(width, height, title)
 
+        self.set_update_rate(1.0 / 60.0)
+
         self.numLEDs = 64
 
         self.pixels = []
 
+        # This needs some cleaning
         for i in range(self.numLEDs):
-            squareSize = (SCREEN_HEIGHT/2) / self.numLEDs
-            pixel = Pixel(squareSize, squareSize, (i+1) * squareSize)            
-            pixel.center_x = SCREEN_WIDTH/2 + i * squareSize - squareSize
-            pixel.center_y = SCREEN_HEIGHT/2 
+            circWidth = (SCREEN_SIZE/2) / self.numLEDs
+            pixel = Pixel(radius=circWidth/2, globalRadius=((i + 1) * circWidth - circWidth/2), color=RGB[i%3])            
+            pixel.center_x = SCREEN_SIZE/2 + i * circWidth + (circWidth/2)
+            pixel.center_y = SCREEN_SIZE/2 
             self.pixels.append(pixel)
 
         # Set background color
@@ -81,13 +75,14 @@ class Rotisplay(arcade.Window):
         # Clear screen
         self.clear()
         # Draw the pixels
+        arcade.draw_circle_outline(SCREEN_SIZE/2, SCREEN_SIZE/2, 3, arcade.color.WHITE)
         for pixel in self.pixels:
             pixel.draw()
 
 
 def main():
     """ Main function """
-    Rotisplay(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
+    Rotisplay(SCREEN_SIZE, SCREEN_SIZE, SCREEN_TITLE)
     arcade.run()
 
 
